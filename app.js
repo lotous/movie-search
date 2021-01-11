10268793766ce28271e8a629715de673f7515a87
etous <elbertjose@hotmail.com>
2021-01-10 19:33:02 -0500

    Change express-hbs for hbs library

--- app.js
+++ app.js
@@ -1,39 +1,31 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
+const hbs=require('hbs');
+const path=require('path');
 const request = require('request');
+
+const viewsPath=path.join(__dirname,'views');
+const partialPath=path.join(__dirname,'views/partials');
 
 const app = express();
 // app.set('view engine', 'ejs');
 
+app.set('viewengine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialPath);
 
-app.engine('hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    registerPartials: './partials',
-    helpers: {
-        block: function (name) {
-            var blocks = this._blocks;
-            content = blocks && blocks[name];
-            return content ? content.join('\n') : null;
-        },
-        contentFor: function (name, options) {
-            var blocks = this._blocks || (this._blocks = {});
-            block = blocks[name] || (blocks[name] = []);
-            block.push(options.fn(this));
-        }
-    }
-}));
-
+// Modification view engine
 app.set('view engine', 'hbs');
 
 
+// Search Route "/"
 app.get('/', function(req, res){
     res.render('search', {
-        layout : 'index',
+        layout : 'layouts/index',
         title: 'Request Movie Page',
     });
 });
 
+// Result Route "/"
 app.get('/results', function(req, res){
     let query = req.query.search;
     let url = 'https://www.omdbapi.com/?apikey=6c0d5126&s=' + query;
@@ -41,7 +33,7 @@ app.get('/results', function(req, res){
         if(!error && response.statusCode === 200){
             let data = JSON.parse(body)
             res.render('results', {
-                layout : 'index',
+                layout : 'layouts/index',
                 title: 'Results Movies Page',
                 movies: data,
             });
